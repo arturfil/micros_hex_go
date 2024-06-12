bcd466815ee976812cb4027261e44b992d4a86c2
arturfil <arturfil@hotmail.com>
2024-06-12 19:16:54 -0400

    error works

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
-	common "github.com/arturfil/m_commons"
+    common "github.com/arturfil/m_commons"
 	pb "github.com/arturfil/m_commons/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type handler struct {
@@ -29,8 +32,44 @@ func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+    if err := validateItems(items); err != nil {
+        common.WriteError(w, http.StatusBadRequest, err.Error())
+    }
+
+    o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
         CustomerID: customerID,
         Items: items,
     })
+
+    rStatus := status.Convert(err)
+    
+    if rStatus != nil {
+        if rStatus.Code() != codes.InvalidArgument {
+            common.WriteError(w, http.StatusBadRequest, rStatus.Message())
+            return 
+        }
+
+        common.WriteError(w, http.StatusInternalServerError, err.Error())
+        return 
+    }
+
+    common.WriteJSON(w, http.StatusOK, o)
+}
+
+func validateItems(items []*pb.ItemsWithQuantity) error {
+    if len(items) == 0 {
+        return errors.New("items must have at least one item")
+    }
+
+    for _, i := range items {
+        if i.ID == "" {
+            return errors.New("item ID is required")
+        }
+
+        if i.Quantity <= 0 {
+            return errors.New("items must have a valid quantity")
+        }
+    }
+
+    return nil
 }
--- orders/grpc_handler.go
+++ orders/grpc_handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+    "fmt"
 
 	pb "github.com/arturfil/m_commons/api"
 	"google.golang.org/grpc"
@@ -18,6 +19,8 @@ func NewGRPCHandler(grpcServer *grpc.Server) {
 }
 
 func (h *grpcHandler) CreateOrder(ctx context.Context, p *pb.CreateOrderRequest) (*pb.Order, error) {
+    return nil, fmt.Errorf("this is an error")
+
     log.Println("New order recieved! Order %v", p)
     o := &pb.Order{
         ID: "42",
